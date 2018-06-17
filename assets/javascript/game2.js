4b1b8022b6b024e4a01a38b3e7adaec4748cc9ea
Daniel An <danielan@Daniels-MacBook-Air.local>
2018-06-17 13:28:39 -0400

    another JS file to expeeriment

---
+++ assets/javascript/game2.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var randomNum = Math.floor(Math.random() * 101) + 19;
+    
+    $("#number").text(randomNum);
+
+    var crystalVal1 = Math.floor(Math.random() * 11) + 1;
+    var crystalVal2 = Math.floor(Math.random() * 11) + 1;
+    var crystalVal3 = Math.floor(Math.random() * 11) + 1;
+    var crystalVal4 = Math.floor(Math.random() * 11) + 1;
+
+    var sum = 0;
+    var wins = 0;
+    var losses = 0;
+
+    $("#win").text(wins);
+    $("#loss").text(losses);
+})
