d1194630fbe68cb362769df53360b312fd9520e4
baehansung <bae.hansung@gmail.com>
2018-06-18 11:57:30 -0400

    JS update

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -3,11 +3,6 @@ $(document).ready(function(){
     
     $("#number").text(randomNum);
 
-    var crystalVal1 = Math.floor(Math.random() * 11) + 1;
-    var crystalVal2 = Math.floor(Math.random() * 11) + 1;
-    var crystalVal3 = Math.floor(Math.random() * 11) + 1;
-    var crystalVal4 = Math.floor(Math.random() * 11) + 1;
-
     var sum = 0;
     var wins = 0;
     var losses = 0;
@@ -15,13 +10,60 @@ $(document).ready(function(){
     $("#win").text(wins);
     $("#loss").text(losses);
 
+    $("#popup").hide();
+
     function reset() {
         $("#number").text(randomNum);
         $("win").text(sum);
+        sum = 0;
+        $("#popup").hide();
     };
 
     function win(){
-        wins++
-        $("#number").text(wins);
+        wins++;
+        $("#win").text(wins);
+        reset();
+        $("#popup").fadeIn(1000);
+        $("#popup").text("You Sir/Ma'am are a Crystal Guru!!" + "<br>" + "The Crystal Village has heard of your legendary skills and are rushing down to make you their King/Queen!!")
+    };
+
+    function loss(){
+        losses++;
+        $("#loss").text(losses);
+        reset();
+        $("#popup").fadeIn(1000);
+    };
+
+    var crystalValues = {};
+    crystalValues[1] = Math.floor(Math.random()*12+1);
+    crystalValues[2] = Math.floor(Math.random()*12+1);
+    crystalValues[3] = Math.floor(Math.random()*12+1);
+    crystalValues[4] = Math.floor(Math.random()*12+1);
+
+    function getCrystal(key){
+        return function(){
+            sum = sum + crystalValues[key];
+            $("#scoreNumber").text(sum);
+
+            if(sum === randomNum){
+                win();
+            } else if(sum > randomNum){
+                loss();
+            }
+        }
     }
+    
+    $("#blue").on("click", getCrystal(1));
+    $("#green").on("click", getCrystal(2));
+    $("#red").on("click", getCrystal(3));
+    $("#yellow").on("click", getCrystal(4));
+
+    // function winPopUp(){
+    //     $("#popup")
+    // }
+
+    // function losePopUp(){
+
+    // }
+
 })
